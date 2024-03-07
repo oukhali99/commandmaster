9774f20782f0cc6703786ea7db0942d71763c784
Neo <neo@mail.com>
2024-03-06 20:11:51 -0500

    Made the ui prettier

--- src/main/java/com/oukhali99/commandmaster/uicontrollers/TextCommandArgumentViewController.java
+++ src/main/java/com/oukhali99/commandmaster/uicontrollers/TextCommandArgumentViewController.java
@@ -4,6 +4,7 @@ import com.oukhali99.commandmaster.model.command.Command;
 import com.oukhali99.commandmaster.model.command.argument.TextCommandArgument;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -14,7 +15,7 @@ public class TextCommandArgumentViewController {
     private TextCommandArgument textCommandArgument;
 
     @FXML
-    private TextField textField;
+    private Label label;
 
     @Autowired
     private CommandListViewController commandListViewController;
@@ -42,7 +43,7 @@ public class TextCommandArgumentViewController {
             return;
         }
 
-        textField.setText(textCommandArgument.getStringValue());
+        label.setText(textCommandArgument.getStringValue());
     }
 
     public void setTextCommandArgument(TextCommandArgument textCommandArgument) {
